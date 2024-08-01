cef740f325778a2922fa1434e9a8fbf795dca3e0
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-31 21:21:32 -0400

    first commit

--- app/my-notes/page.tsx
+++ app/my-notes/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
-
-const page = () => {
-  return <div>page</div>;
-};
-
-export default page;
+export default function page() {
+  return (
+    <>
+      <div>
+        <UserButton />
+      </div>
+    </>
+  );
+}
