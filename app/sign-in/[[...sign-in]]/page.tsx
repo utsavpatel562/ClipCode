f89270ad9775a80c58752b8083fefaf2d40629f8
utsavpatel562 <utsavpatel562@gmail.com>
2024-07-31 21:04:21 -0400

    first commit

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { SignIn, useUser } from "@clerk/nextjs";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const SignedInPage = () => {
+  return (
+    <>
+      <div className={`w-full h-screen flex justify-center items-center`}>
+        <SignIn />
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default SignedInPage;
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const SignedUpPage = () => {
+  return (
+    <>
+      <div className={`w-full h-screen flex justify-center items-center`}>
+        <SignUp />
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default SignedUpPage;
