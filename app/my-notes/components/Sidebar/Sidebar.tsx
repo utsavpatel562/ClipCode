f4deba2087f39edb85414fa640ec63d46fb0b347
utsavpatel562 <utsavpatel562@gmail.com>
2024-08-03 13:46:30 -0400

    first commit

--- app/my-notes/components/Sidebar/Sidebar.tsx
+++ app/my-notes/components/Sidebar/Sidebar.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import Code from "@mui/icons-material/Code";
 import {
   IconBorderAll,
+  IconBrandJavascript,
+  IconBrandPython,
+  IconBrandTypescript,
   IconHeart,
-  IconLogout,
   IconSettings,
   IconTrash,
 } from "@tabler/icons-react";
@@ -82,6 +84,26 @@ function Languages() {
     <>
       <div className="mt-10 text-base">
         <div className="font-bold text-slate-400">Languages</div>
+        <div className="mt-5 ml-2 text-slate-400 flex flex-col gap-4">
+          <div className="flex justify-between">
+            <div className="flex gap-1 items-center">
+              <IconBrandJavascript style={{ fontSize: 15 }} /> JavaScript
+            </div>
+            <span className="font-bold">3</span>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex gap-1 items-center">
+              <IconBrandPython style={{ fontSize: 15 }} /> Python
+            </div>
+            <span className="font-bold">11</span>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex gap-1 items-center">
+              <IconBrandTypescript style={{ fontSize: 15 }} /> TypeScript
+            </div>
+            <span className="font-bold">9</span>
+          </div>
+        </div>
       </div>
     </>
   );
