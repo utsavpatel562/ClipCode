72920b5d22ad7dfd31baa52a7cc25df13afa315c
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-08-11 20:28:39 -0400

    Update TypewriterEffect.tsx

--- app/components/TypewriterEffect.tsx
+++ app/components/TypewriterEffect.tsx
@@ -24,7 +24,7 @@ export function TypewriterEffectSmoothDemo() {
     },
   ];
   return (
-    // Code By Utsav 
+    // Code By Utsav for TypeWriterEffect
     <div className="flex flex-col items-center justify-center">
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 text-gray-400 text-center text-[17px]">
